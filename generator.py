4e7ef5fdcc97e43395137095d0f996b0e6366459
iommu <acarter010204@gmail.com>
2019-07-19 10:11:24 +1000

    added initial file for TensorFlow generator

---
+++ generator.py
@@ -0,0 +1,12 @@
+# TensorFlow and Keras
+import tensorflow as tf
+from tensorflow import keras
+# Helper Libs
+import numpy as np
+
+tf.enable_eager_execution()
+
+print("TF version : {}".format(tf.__version__))
+print("Eager Exec : {}".format(tf.executing_eagerly()))
+
+
