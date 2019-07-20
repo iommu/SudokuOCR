53c41940af112ad31104c5ec9a6bbe89eb49987e
Alex Carter <acarter010204@gmail.com>
2019-07-21 00:17:27 +1000

    convert to tflite

--- main.sh
+++ main.sh
@@ -9,3 +9,10 @@ python3 ./xml_to_dir.py -in-xml annotations -in-img images -out data
 
 # run retrain script
 python3 retrain.py --image_dir ./data
+
+# convert to tflite from .pb
+toco \
+  --graph_def_file=/tmp/output_graph.pb \
+  --output_file=./output/output_graph.tflite \
+  --input_array=Placeholder \
+  --output_array=final_result
